36a0fea0126f8f67bb5cc31093278a35a3023fa8
CAPER TECH KENYA <caspertechke@gmail.com>
2025-10-19 00:11:36 +0300

    POWERED BY 𝖢𝖠𝖲𝖯𝖤𝖱 𝖳𝖤𝖢𝖧

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+import Layout from '../components/Layout'
+
+export default function About() {
+  return (
+    <Layout>
+      <div className="page-content">
+        <h1>About Us</h1>
+        <p>We are passionate about bringing you the best music experience.</p>
+      </div>
+    </Layout>
+  )
+}
