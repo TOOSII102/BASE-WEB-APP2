aa824b6ccbd869deb7e47b16dc997319914eb3e8
CAPER TECH KENYA <caspertechke@gmail.com>
2025-10-19 00:15:03 +0300

    POWERED BY 𝖢𝖠𝖲𝖯𝖤𝖱 𝖳𝖤𝖢𝖧

---
+++ components/AnimatedBackground.js
@@ -0,0 +1,73 @@
+import { useEffect, useRef } from 'react'
+
+export default function AnimatedBackground() {
+  const canvasRef = useRef(null)
+
+  useEffect(() => {
+    const canvas = canvasRef.current
+    const ctx = canvas.getContext('2d')
+    let animationFrameId
+
+    const resizeCanvas = () => {
+      canvas.width = window.innerWidth
+      canvas.height = window.innerHeight
+    }
+
+    resizeCanvas()
+    window.addEventListener('resize', resizeCanvas)
+
+    class Particle {
+      constructor() {
+        this.x = Math.random() * canvas.width
+        this.y = Math.random() * canvas.height
+        this.size = Math.random() * 2 + 1
+        this.speedX = Math.random() * 1 - 0.5
+        this.speedY = Math.random() * 1 - 0.5
+        this.color = `hsl(${Math.random() * 360}, 50%, 60%)`
+      }
+
+      update() {
+        this.x += this.speedX
+        this.y += this.speedY
+
+        if (this.x > canvas.width) this.x = 0
+        else if (this.x < 0) this.x = canvas.width
+        if (this.y > canvas.height) this.y = 0
+        else if (this.y < 0) this.y = canvas.height
+      }
+
+      draw() {
+        ctx.fillStyle = this.color
+        ctx.beginPath()
+        ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2)
+        ctx.fill()
+      }
+    }
+
+    const particles = []
+    for (let i = 0; i < 50; i++) {
+      particles.push(new Particle())
+    }
+
+    const animate = () => {
+      ctx.fillStyle = 'rgba(10, 10, 20, 0.1)'
+      ctx.fillRect(0, 0, canvas.width, canvas.height)
+
+      particles.forEach(particle => {
+        particle.update()
+        particle.draw()
+      })
+
+      animationFrameId = requestAnimationFrame(animate)
+    }
+
+    animate()
+
+    return () => {
+      cancelAnimationFrame(animationFrameId)
+      window.removeEventListener('resize', resizeCanvas)
+    }
+  }, [])
+
+  return <canvas ref={canvasRef} className="animated-background" />
+}
