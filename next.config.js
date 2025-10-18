3040bebfd2db348ff70428b5fef83e83aaa9aa16
CAPER TECH KENYA <caspertechke@gmail.com>
2025-10-19 00:29:13 +0300

    POWERED BY 𝖢𝖠𝖲𝖯𝖤𝖱 𝖳𝖤𝖢𝖧

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    appDir: false, // Set to false if using pages directory
+  },
+  trailingSlash: false,
+}
+
+module.exports = nextConfig
